8e0454d64f77acf2d6312997d09723edbacd98e5
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-20 11:06:43 +0200

    GlobalQuoteResponse data model

---
+++ StocksApp/Features/Quotes/Models/GlobalQuote.swift
@@ -0,0 +1,36 @@
+//
+//  GlobalQuote.swift
+//  StocksApp
+//
+//  Created by Ramill Ibragimov on 20.02.2021.
+//
+
+import Foundation
+
+struct GlobalQuoteResponse: Codable {
+    var quote: Quote
+    
+    private enum CodingKeys: String, CodingKey {
+        case quote = "Global Quote"
+    }
+}
+
+struct Quote: Codable {
+    var symbol: String
+    var open: String
+    var high: String
+    var low: String
+    var price: String
+    var change: String
+    var changePercent: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case symbol = "01. symbol"
+        case open = "02. open"
+        case high = "03. high"
+        case low = "04. low"
+        case price = "05. price"
+        case change = "09. change"
+        case changePercent = "10. change percent"
+    }
+}
