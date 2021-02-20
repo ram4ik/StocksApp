1cd56ee74a4d9bdc08df5b1b55f163842b2a4e97
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-20 10:16:42 +0200

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Ramill Ibragimov on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
