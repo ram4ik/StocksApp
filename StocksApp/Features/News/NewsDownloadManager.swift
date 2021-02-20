7ade226c6e135eafe63fb490d6639a8604b7500b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-20 17:05:46 +0200

    NewsDownloadManager

---
+++ StocksApp/Features/News/NewsDownloadManager.swift
@@ -0,0 +1,32 @@
+//
+//  NewsDownloadManager.swift
+//  StocksApp
+//
+//  Created by Ramill Ibragimov on 20.02.2021.
+//
+
+import Foundation
+
+final class NewsDownloadManager: ObservableObject {
+    
+    @Published var newsArticles = [News]()
+    
+    private let newsUrlString = "https://newsapi.org/v2/top-headline?country=us&category=business&apiKey=\(NewsAPI.key)"
+    
+    init() {
+        download()
+    }
+    
+    func download() {
+        NetworkManager<NewsResponse>().fetch(from: URL(string: newsUrlString)!) { (result) in
+            switch result {
+            case .failure(let err):
+                print(err)
+            case .success(let resp):
+                DispatchQueue.main.async {
+                    self.newsArticles = resp.articles
+                }
+            }
+        }
+    }
+}
